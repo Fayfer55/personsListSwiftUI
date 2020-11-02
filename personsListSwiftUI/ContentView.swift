2f3552bbd2acb7c70b54990851083053176654d8
Fayfer Kirill <sayfer555@gmail.com>
2020-11-02 19:23:06 +0600

    Initial Commit

---
+++ personsListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  personsListSwiftUI
+//
+//  Created by Кирилл Файфер on 02.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ personsListSwiftUI/personsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  personsListSwiftUIApp.swift
+//  personsListSwiftUI
+//
+//  Created by Кирилл Файфер on 02.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct personsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
